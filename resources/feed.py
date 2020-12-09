767796f60b3b65e22441f98804cd13d861aa24c4
Gabriel Jones <gabrieljaredjones@gmail.com>
2020-12-09 01:28:24 +0000

    url testing

--- resources/feed.py
+++ resources/feed.py
@@ -13,21 +13,21 @@ class Restaurants(Resource):
                     {
                         'id': 1,
                         'thumbnail_url': '',
-                        'video_url': 'https://www.youtube.com/watch?v=SGyVUNUnUQk',
+                        'video_url': 'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4',
                         'title': 'Clip title',
                         'description': 'lorem ipsum dolem amet #food #seafood'
                     },
                     {
                         'id': 2,
                         'thumbnail_url': '',
-                        'video_url': 'https://www.youtube.com/watch?v=SGyVUNUnUQk',
+                        'video_url': 'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerFun.mp4',
                         'title': 'Another clip',
                         'description': 'describing the food #breakfast #morningroll #bacon'
                     },
                     {
                         'id': 3,
                         'thumbnail_url': '',
-                        'video_url': 'https://www.youtube.com/watch?v=SGyVUNUnUQk',
+                        'video_url': 'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerJoyrides.mp4',
                         'title': 'Third clip',
                         'description': 'description here #sushi #asian'
                     }
