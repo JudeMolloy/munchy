21dfff78b8742c1b83ff0d630b6fca4d61c53bd0
Jude Molloy <JudeMolloy07@hotmail.com>
2020-12-15 19:27:30 +0000

    testing hls file.

--- resources/feed.py
+++ resources/feed.py
@@ -20,7 +20,7 @@ class Restaurants(Resource):
                     {
                         'id': 2,
                         'thumbnail_url': '',
-                        'video_url': 'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerFun.mp4',
+                        'video_url': 'https://d11rse4z1ry9t6.cloudfront.net/12da932b-cfd1-4208-ab9d-e05d7140bc3c/AppleHLS1/Fireaway_Pizza_Trim.m3u8',
                         'title': 'Another clip',
                         'description': 'describing the food #breakfast #morningroll #bacon'
                     },
