fe375c719729d6406090c31e26bffd37009c7c39
Jude Molloy <JudeMolloy07@hotmail.com>
2020-12-16 04:30:13 +0000

    When file is uploaded to AWS bucket it is now converted into HLS file and served through cloudfront. Can compute the link and next step is to integrate these additions to the database so everything can work together as required.

--- resources/upload.py
+++ resources/upload.py
@@ -1,4 +1,6 @@
-import os, boto3
+import boto3
+import uuid
+import os
 
 from flask import request, render_template, make_response
 from flask_restful import Resource
@@ -24,15 +26,27 @@ class Upload(Resource):
     def post(cls):
         img = request.files["file"]
         if img:
-            filename = secure_filename(img.filename)
-            img.save(filename)
-            key = "assets01/" + filename # Will upload into specific folder.
+            unique = str(uuid.uuid4())
+            split_name = img.filename.split(".")
+            name = split_name[0]
+            file_ext = split_name[1]
+
+            unique_name = secure_filename(unique + "-" + name)  # Generates unique name.
+            unique_filename = unique_name + "." + file_ext  # Generates video filename.
+            m3u8_filename = unique_name + ".m3u8"  # Generates HLS filename.
+
+            img.save(unique_filename)
+
+            key = "assets01/" + unique_filename  # Uploads into specific folder to trigger the AWS Lambda function.
             s3.upload_file(
                 Bucket=BUCKET_NAME,
-                Filename=filename,
+                Filename=unique_filename,
                 Key=key
             )
             # Remove the file from the local file system after uploading to s3.
-            os.remove(filename)
+            os.remove(unique_filename)
+            # Generates the link to the cdn which the frontend can usses to pull the on demand video.
+            cdn_link = "https://d11rse4z1ry9t6.cloudfront.net/" + unique_name + "/AppleHLS1/" + m3u8_filename
+            print(cdn_link)
             return make_response(render_template("upload-success.html", response="successful"))
         return make_response(render_template("upload-success.html", response="unsuccessful"))
