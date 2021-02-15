2bc3926aebb9f419243bc91c73e1cf57305b2bcc
Jude Molloy <JudeMolloy07@hotmail.com>
2021-02-15 00:15:59 +0000

    Still trying to fix redis problem.

--- blacklist.py
+++ blacklist.py
@@ -1,13 +1,16 @@
 # Requires Redis server to be running.
 import os
-
 import redis
 
+from urllib.parse import urlparse
 from datetime import timedelta
 
 # Values can be changed.
 ACCESS_EXPIRES = timedelta(minutes=15)
 REFRESH_EXPIRES = timedelta(days=30)
 
+url = urlparse(os.environ.get("REDIS_URL"))
+r = redis.Redis(host=url.hostname, port=url.port, username=url.username, password=url.password, ssl=True, ssl_cert_reqs=None)
+
 # Setup the redis connection for storing the blacklisted tokens
-revoked_store = redis.StrictRedis(host=os.environ.get('REDIS_URL'), port=6379, db=0, decode_responses=True)
+#revoked_store = redis.StrictRedis(host=os.environ.get('REDIS_URL'), port=6379, db=0, decode_responses=True)
