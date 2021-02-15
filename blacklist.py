73c2f9b6d2746de7562780a78b855885671c0d00
Jude Molloy <JudeMolloy07@hotmail.com>
2021-02-15 00:18:09 +0000

    minor.

--- blacklist.py
+++ blacklist.py
@@ -10,7 +10,7 @@ ACCESS_EXPIRES = timedelta(minutes=15)
 REFRESH_EXPIRES = timedelta(days=30)
 
 url = urlparse(os.environ.get("REDIS_URL"))
-r = redis.Redis(host=url.hostname, port=url.port, username=url.username, password=url.password, ssl=True, ssl_cert_reqs=None)
+revoked_store = redis.StrictRedis(host=url.hostname, port=url.port, username=url.username, password=url.password, ssl=True, ssl_cert_reqs=None, decode_responses=True)
 
 # Setup the redis connection for storing the blacklisted tokens
 #revoked_store = redis.StrictRedis(host=os.environ.get('REDIS_URL'), port=6379, db=0, decode_responses=True)
